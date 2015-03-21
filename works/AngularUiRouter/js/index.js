2e474efb4bb28dbd0a3aeeff4b75f4387470d5ba
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-03-21 22:16:29 +0800

    ui-router

--- works/AngularUiRouter/js/index.js
+++ works/AngularUiRouter/js/index.js
@@ -1,20 +1,26 @@
 /**
-* routerApp Module
-*
-* Description
-* Just test ui-router
-*/
+ * routerApp Module
+ *
+ * Description
+ * Just test ui-router
+ */
 angular.module('routerApp', ['ui.router'])
 	.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/index');
-	$stateProvider
-		.state('index',{
-			url:'/index',
-			views: {
-				'':{
-					templateUrl:'templates/index.html'
-				},
-				
-			}
-		});
+	    $urlRouterProvider.otherwise('/index');
+	    $stateProvider
+		    .state('index', {
+		        url: '/index',
+		        views: {
+		            '': {
+		                templateUrl: 'templates/AllContent.html'
+		            },
+		            'navbar@index':{
+		            	templateUrl: 'templates/navbar.html'
+		            },
+		            'main@index':{
+		            	templateUrl: 'templates/home.html'
+		            }
+
+		        }
+		    });
 });
