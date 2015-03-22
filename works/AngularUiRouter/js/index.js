d27c429f6a7d6fe6d0c96e165fb4a2f7ba3cc99b
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-03-22 20:58:18 +0800

    ui-router:practice done

--- works/AngularUiRouter/js/index.js
+++ works/AngularUiRouter/js/index.js
@@ -5,29 +5,61 @@
  * Just test ui-router
  */
 angular.module('routerApp', ['ui.router'])
-	.config(function($stateProvider, $urlRouterProvider) {
-	    $urlRouterProvider.otherwise('/index');
-	    $stateProvider
-		    .state('index', {
-		        url: '/index',
-		        views: {
-		            '': {
-		                templateUrl: 'templates/AllContent.html'
-		            },
-		            'navbar@index':{
-		            	templateUrl: 'templates/navbar.html'
-		            },
-		            'main@index':{
-		            	templateUrl: 'templates/home.html'
-		            }
-		        }
-		    })
-		    .state('index.blog', {
-		    	url: '/blog',
-		    	views:{
-		    		'main@index':{
-		    			templateUrl:'templates/blogs.html'
-		    		}
-		    	}
-		    });
-});
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/index');
+        $stateProvider
+            .state('index', {
+                url: '/index',
+                views: {
+                    '': {
+                        templateUrl: 'templates/AllContent.html'
+                    },
+                    'navbar@index': {
+                        templateUrl: 'templates/navbar.html'
+                    },
+                    'main@index': {
+                        templateUrl: 'templates/home.html'
+                    }
+                }
+            })
+            .state('index.blog', {
+                url: '/blog',
+                views: {
+                    'main@index': {
+                        templateUrl: 'templates/blog.html'
+                    }
+                }
+            })
+            .state('index.blog.Jekyll', {
+                url: '/Jekyll',
+                templateUrl: 'templates/Jekyll.html'
+            })
+            .state('index.blog.GitHub', {
+                url: '/GitHub',
+                templateUrl: 'templates/GitHub.html'
+            })
+            .state('index.blog.Html', {
+                url: '/Html',
+                templateUrl: 'templates/Html.html'
+            })
+            .state('index.blog.CSS', {
+                url: '/CSS',
+                templateUrl: 'templates/CSS.html'
+            })
+            .state('index.demo', {
+                url: '/demo',
+                views: {
+                    'main@index': {
+                        templateUrl: 'templates/demo.html'
+                    }
+                }
+            })
+            .state('index.practice',{
+                url:'/practice',
+                views:{
+                    'main@index':{
+                        templateUrl:'templates/practice.html'
+                    }
+                }
+            });
+    });
