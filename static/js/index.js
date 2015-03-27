534ff4ba86f74910667370c2c3edf21d25b99784
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-03-27 21:18:07 +0800

    fix footer

---
+++ static/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    /*footer position suitable*/
+    fixfooter();
+    $(window).resize(function() {
+        fixfooter();
+    });
+
+});
+
+function fixfooter() {
+    var windowHeight = $(window).height();
+    var contentHeight = $('body>.container').outerHeight() + $('body>.container').offset().top + 50 + 30;
+    if (contentHeight < windowHeight) {
+        $('footer').addClass('navbar-fixed-bottom');
+    } else {
+        $('footer').removeClass('navbar-fixed-bottom');
+    }
+}
