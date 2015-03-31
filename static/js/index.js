e86a2ee34ce3e2b59a0dd2e79716623c8418092b
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-03-31 14:15:35 +0800

    change speed

--- static/js/index.js
+++ static/js/index.js
@@ -49,8 +49,8 @@ function categoryDisplay() {
     $('.categories-list-item').click(function() {
         var cate = $(this).attr('cate');//get category's name
         
-        $('.post-list-body>div[post-cate!='+cate+']').hide(300);
-        $('.post-list-body>div[post-cate='+cate+']').show(300);
+        $('.post-list-body>div[post-cate!='+cate+']').hide(250);
+        $('.post-list-body>div[post-cate='+cate+']').show(400);
 
     });
 }
