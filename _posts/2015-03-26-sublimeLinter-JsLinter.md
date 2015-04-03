a1ce6129f899663e72d0836d14d67aba6b09bd2d
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-04-03 21:35:34 +0800

    new post

---
+++ _posts/2015-03-26-sublimeLinter-JsLinter.md
@@ -0,0 +1,19 @@
+---
+layout: post
+title:  "代码校验工具SublimeLinter的安装与使用"
+date:   2015-03-26 15:14:54
+categories: Sublime
+excerpt: 代码校验工具sublimeLinter的安装与使用，JSLinter的安装与使用
+---
+
+## 序   
+
+本文我将讲述一下`SublimeLinter`的安装过程，以及其组件 JavaScriptLinter 的安装于使用。我将基于 [Sublime Text 3](http://sublimetext.com/3) 来安装。使用 Sublime Text 2 的用户阅读本文是没有帮助的。   
+
+SublimeLinter是Sublime的插件，它的作用是检查代码语法是否有错误，并提示。习惯了IDE下写代码的人一定需要一款在Sublime上类似的语法检查工具。下面我们开始。   
+
+## 安装 SublimeLinter   
+
+如同其他插件一样使用 Package Control 来安装，使用 `Ctrl+Shift+p` 进入 Package Control: Install Package，输入 SublimeLinter。进行安装.
+
+# 未完待续...
--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,29 @@
-$(document).ready(function() {
 
-    fixFooterInit();
+$(document).ready(function() {
 
     categoryDisplay();
 
 });
 
+/**
+ * load方法，页面的加载完成后触发
+ * @param  {[type]} ) {fixFooterInit();} 固定Footer栏
+ * @return {[type]}   
+ */
+$(window).load(function() {
+    fixFooterInit();
+});
+
+/**
+ * 固定底栏的初始化方法
+ * 在一开始载入页面时，使用fixFooter()方法固定底栏。
+ * 在浏览器窗口改变大小是，依然固定底栏
+ * @return {[type]} [description]
+ */
 function fixFooterInit() {
     var footerHeight = $('footer').outerHeight();
-    /*var footerMarginTop = getFooterMarginTop();*/
-    var footerMarginTop = 80;
+    var footerMarginTop = getFooterMarginTop()-0;//类型转换
+    // var footerMarginTop = 80;
     
     fixFooter(footerHeight,footerMarginTop);//fix footer at the beginning
 
@@ -18,30 +32,50 @@ function fixFooterInit() {
     });
 }
 
-/* fix footer */
+/**
+ * 固定底栏
+ * @param  {number} footerHeight    底栏高度
+ * @param  {number} footerMarginTop 底栏MarginTop
+ * @return {[type]}                 [description]
+ */
 function fixFooter(footerHeight,footerMarginTop) {
     var windowHeight = $(window).height();
     var contentHeight = $('body>.container').outerHeight() + $('body>.container').offset().top + footerHeight + footerMarginTop;
+    // console.log("window---"+windowHeight);
+    // console.log("$('body>.container').outerHeight()---"+$('body>.container').outerHeight() );
+    // console.log("$('body>.container').height()---"+$('body>.container').height() );
+    // console.log("$('#main').height()--------"+$('#main').height());
+    // console.log("$('body').height()--------"+$('body').height());
+    //console.log("$('#main').html()--------"+$('#main').html());
+    // console.log("$('body>.container').offset().top----"+$('body>.container').offset().top);
+    // console.log("footerHeight---"+footerHeight);
+    // console.log("footerMarginTop---"+footerMarginTop);
+    // console.log(contentHeight);
     if (contentHeight < windowHeight) {
         $('footer').addClass('navbar-fixed-bottom');
     } else {
         $('footer').removeClass('navbar-fixed-bottom');
     }
 }
-/*RegExp for fixFooter
-* I don't know why there is a bug. 
-* When you resize the height of the window to the bottom of container, 
-* there is a throb of the footer.
-* So the code below I don't use now.
-*/
-/*function getFooterMarginTop() {
+
+/**
+ * 使用正则表达式得到底栏的MarginTop
+ * @return {string} 底栏的MarginTop
+ */
+function getFooterMarginTop() {
     var margintop = $('footer').css('marginTop');
     var patt = new RegExp("[0-9]*");
     var re = patt.exec(margintop);
-    console.log(re[0]);
+    // console.log(re[0]);
     return re[0];
-}*/
+}
 
+/**
+ * 分类展示
+ * 点击右侧的分类展示时
+ * 左侧的相关裂变展开或者收起
+ * @return {[type]} [description]
+ */
 function categoryDisplay() {
     /*only show All*/
     $('.post-list-body>div[post-cate!=All]').hide();
