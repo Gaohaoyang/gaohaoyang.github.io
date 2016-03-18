8f4b2ac31e4f95a386f03dc06ecb1fe468ecd6dc
Gaohaoyang <gaohaoyang126@126.com>
2016-03-18 15:37:14 +0800

    add cancle bubble in menu

--- js/main.js
+++ js/main.js
@@ -3,44 +3,6 @@
  */
 /* jshint asi:true */
 
-// ////////////////////common util////////////////////////////////
-//
-// /**
-//  * [fadeOut description]
-//  * @param  {element} el element
-//  * @return {[type]}    [description]
-//  */
-// function fadeOut(el) {
-//     el.style.opacity = 1;
-//
-//     (function fade() {
-//         if ((el.style.opacity -= 0.1) < 0) {
-//             el.style.display = "none";
-//         } else {
-//             requestAnimationFrame(fade);
-//         }
-//     })();
-// }
-//
-// /**
-//  * [fadeIn description]
-//  * @param  {element} el      [description]
-//  * @param  {property} display [description]
-//  * @return {[type]}         [description]
-//  */
-// function fadeIn(el, display) {
-//     el.style.opacity = 0;
-//     el.style.display = display || "block";
-//
-//     (function fade() {
-//         var val = parseFloat(el.style.opacity);
-//         if (!((val += 0.1) > 1)) {
-//             el.style.opacity = val;
-//             requestAnimationFrame(fade);
-//         }
-//     })();
-// }
-
 /////////////////////////header////////////////////////////
 headerFun();
 
@@ -57,7 +19,8 @@ function headerFun() {
 function _clickMenu() {
     var menuBtn = document.querySelector('#headerMenu')
     var nav =  document.querySelector('#headerNav')
-    menuBtn.onclick = function() {
+    menuBtn.onclick = function(e) {
+        e.stopPropagation()
         if (menuBtn.classList.contains('active')) {
             // fadeOut(nav)
             nav.style.display = 'none'
@@ -67,5 +30,9 @@ function _clickMenu() {
             nav.style.display = 'block'
             menuBtn.classList.add('active')
         }
+    }
+    document.querySelector('body').onclick = function () {
+        nav.style.display = 'none'
+        menuBtn.classList.remove('active')
     }
 }
