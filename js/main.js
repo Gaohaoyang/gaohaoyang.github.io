0f0ff0cacde10b70a4d49a4e10aa9e0ffafc785c
Gaohaoyang <gaohaoyang126@126.com>
2016-03-19 22:57:11 +0800

    sidebar done without fixed

--- js/main.js
+++ js/main.js
@@ -38,3 +38,29 @@ function _clickMenu() {
         }
     }
 }
+
+/////////////////////////content////////////////////////////
+generateContent()
+/**
+ * [generateContent description]
+ */
+function generateContent() {
+    var contentToc = document.querySelector('#markdown-toc')
+    if (contentToc === null) {
+        document.querySelector('#content').style.display = 'none'
+    } else {
+        var contentHtml = contentToc.innerHTML
+        var sideContentUl = document.querySelector('#content-side')
+        sideContentUl.innerHTML = contentHtml
+    }
+
+    // 若无相似文章，隐藏这部分
+    var relatedPost = document.querySelector('.related-post');
+    if (relatedPost.innerHTML.trim()==='') {
+        relatedPost.style.display='none'
+    }
+}
+
+function fixSidebar() {
+
+}
