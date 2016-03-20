62530143beed9976ba669bdcaf1af9f202eece97
Gaohaoyang <gaohaoyang126@126.com>
2016-03-20 18:01:24 +0800

    sidebar done

---
+++ js/scroll.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+
+  const root = (() => {
+    if ("scrollingElement" in document) return document.scrollingElement;
+    const html = document.documentElement;
+    const start = html.scrollTop;
+    html.scrollTop = start + 1;
+    const end = html.scrollTop;
+    html.scrollTop = start;
+    return end > start ? html : document.body;
+  })();
+
+  const ease = (duration, elapsed, start, end) =>
+    Math.round(end * (-Math.pow(2, -10 * elapsed/duration) + 1) + start);
+
+  const getCoordinates = hash => {
+    const start = root.scrollTop;
+    const delta = (() => {
+      if (hash.length < 2) return -start;
+      const target = document.querySelector(hash);
+      if (!target) return;
+      const top = target.getBoundingClientRect().top;
+      const max = root.scrollHeight - window.innerHeight;
+      return start + top < max ? top : max - start;
+    })();
+    if (delta) return new Map([["start", start], ["delta", delta]]);
+  };
+
+  const scroll = link => {
+    const hash = link.getAttribute("href");
+    const coordinates = getCoordinates(hash);
+    if (!coordinates) return;
+
+    const tick = timestamp => {
+      progress.set("elapsed", timestamp - start);
+      root.scrollTop = ease(...progress.values(), ...coordinates.values());
+      progress.get("elapsed") < progress.get("duration")
+      ? requestAnimationFrame(tick)
+      : complete(hash, coordinates);
+    };
+
+    const progress = new Map([["duration", 800]]);
+    const start = performance.now();
+    requestAnimationFrame(tick);
+  };
+
+  const complete = (hash, coordinates) => {
+    history.pushState(null, null, hash);
+    root.scrollTop = coordinates.get("start") + coordinates.get("delta");
+  };
+
+  const last = list => list.length - 1;
+
+  const attachHandler = (links, index = last(links)) => {
+    const link = links.item(index);
+    link.addEventListener("click", event => {
+      event.preventDefault();
+      scroll(link);
+    });
+    if (index) return attachHandler(links, index - 1);
+  };
+
+  attachHandler(document.querySelectorAll("a.scroll"));
+});
