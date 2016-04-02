54d0c62b47866ab1754a3ce493f9de0cb79e3fd9
Gao Haoyang <gaohaoyang126@126.com>
2016-04-02 21:25:54 +0800

    adjust header menu animate

--- js/main.js
+++ js/main.js
@@ -8,23 +8,21 @@
  * clickMenu
  */
 (function() {
-    var menuBtn = document.querySelector('#headerMenu')
-    var nav = document.querySelector('#headerNav')
-    menuBtn.onclick = function(e) {
-        e.stopPropagation()
-        if (menuBtn.classList.contains('active')) {
-            // fadeOut(nav)
-            nav.style.display = 'none'
-            menuBtn.classList.remove('active')
-        } else {
-            // fadeIn(nav)
-            nav.style.display = 'block'
-            menuBtn.classList.add('active')
-        }
-    }
     if (window.innerWidth <= 770) {
+        var menuBtn = document.querySelector('#headerMenu')
+        var nav = document.querySelector('#headerNav')
+        menuBtn.onclick = function(e) {
+            e.stopPropagation()
+            if (menuBtn.classList.contains('active')) {
+                menuBtn.classList.remove('active')
+                nav.classList.remove('nav-show')
+            } else {
+                nav.classList.add('nav-show')
+                menuBtn.classList.add('active')
+            }
+        }
         document.querySelector('body').addEventListener('click', function() {
-            nav.style.display = 'none'
+            nav.classList.remove('nav-show')
             menuBtn.classList.remove('active')
         })
     }
