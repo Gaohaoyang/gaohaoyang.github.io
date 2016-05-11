9bc1c273cb3f387dde0abf91ac5274c1220814aa
Gaohaoyang <gaohaoyang126@126.com>
2016-05-11 15:31:58 +0800

    add animation of scroll

--- js/main.js
+++ js/main.js
@@ -32,16 +32,23 @@
 //////////////////////////back to top////////////////////////////
 (function() {
     var backToTop = document.querySelector('.back-to-top')
-    console.log(backToTop);
-    window.onscroll = function() {
+    var backToTopA = document.querySelector('.back-to-top a')
+        // console.log(backToTop);
+    window.addEventListener('scroll',function () {
+
 
         // 页面顶部滚进去的距离
         var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
 
-        if (scrollTop>200) {
+        if (scrollTop > 200) {
             backToTop.classList.add('back-to-top-show')
-        }else{
+        } else {
             backToTop.classList.remove('back-to-top-show')
         }
-    }
+    })
+
+    // backToTopA.addEventListener('click',function (e) {
+    //     e.preventDefault()
+    //     window.scrollTo(0,0)
+    // })
 }());
