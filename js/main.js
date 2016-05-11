f369fe0227644f5b272f51c13473718be120f3ba
Gaohaoyang <gaohaoyang126@126.com>
2016-05-11 15:02:25 +0800

    back to top start

--- js/main.js
+++ js/main.js
@@ -27,3 +27,21 @@
         })
     }
 }());
+
+
+//////////////////////////back to top////////////////////////////
+(function() {
+    var backToTop = document.querySelector('.back-to-top')
+    console.log(backToTop);
+    window.onscroll = function() {
+
+        // 页面顶部滚进去的距离
+        var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+
+        if (scrollTop>200) {
+            backToTop.classList.add('back-to-top-show')
+        }else{
+            backToTop.classList.remove('back-to-top-show')
+        }
+    }
+}());
